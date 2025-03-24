57107adb2986aa8afb7696a590520c4ffecdb648
Steffan Troxel <sjtroxel@alumni.iu.edu>
2025-03-23 20:42:54 -0500

    investment-calculator works!

---
+++ 06-starting-project/src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,38 @@
+import { CurrencyPipe } from '@angular/common';
+import { Component, computed, inject, Input, input } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CurrencyPipe],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css'
+})
+export class InvestmentResultsComponent {
+  private investmentService = inject(InvestmentService);
+  // results = input<{
+  //   year: number,
+  //   interest: number,
+  //   valueEndOfYear: number,
+  //   annualInvestment: number,
+  //   totalInterest: number,
+  //   totalAmountInvested: number,
+  // }[]>()
+  // @Input() results?: {
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }[];
+
+  results = computed(() => this.investmentService.resultData());
+
+      // results = this.investmentService.resultData.asReadonly();
+
+  // get results() {
+  //   return this.investmentService.resultData;
+  // }
+}
---
+++ 06-starting-project/src/app/user-input/user-input.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, output, Output, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css'
+})
+export class UserInputComponent {
+  // calculate = output<InvestmentInput>();
+  enteredInitialInvestment = signal('0');
+  enteredAnnualInvestment = signal('0');
+  enteredExpectedReturn = signal('5');
+  enteredDuration = signal('10');
+
+  constructor(private investmentService: InvestmentService) {}
+
+  onSubmit() {
+    this.investmentService.calculateInvestmentResults({
+      initialInvestment: +this.enteredInitialInvestment(),
+      duration: +this.enteredDuration(),
+      expectedReturn: +this.enteredExpectedReturn(),
+      annualInvestment: +this.enteredAnnualInvestment()
+    });
+    // this.calculate.emit();
+    this.enteredInitialInvestment.set('0');
+    this.enteredAnnualInvestment.set('0');
+    this.enteredExpectedReturn.set('5');
+    this.enteredDuration.set('10');
+  }
+}
