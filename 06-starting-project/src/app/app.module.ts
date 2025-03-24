e82f317525c2474b98f0277bc08f94969410f34a
Steffan Troxel <sjtroxel@alumni.iu.edu>
2025-03-23 21:35:45 -0500

    practice time is over and sleep time is now.

---
+++ 06-starting-project/src/app/app.module.ts
@@ -0,0 +1,21 @@
+
+    // *** I'm going to stop coding along here and just watch the rest of the video tonight because I'd rather leave these examples here for me to look back on all set as standalone components, not as the modular system since I'm told it's less modern/less important for me to know moving forward... --ST  ***
+
+
+// import { NgModule } from "@angular/core";
+// import { HeaderComponent } from "./header/header.component";
+// import { AppComponent } from "./app.component";
+// import { UserInputComponent } from "./user-input/user-input.component";
+// import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
+
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     HeaderComponent,
+//     UserInputComponent,
+//     InvestmentResultsComponent
+//   ]
+// })
+
+// export class AppModule {}
